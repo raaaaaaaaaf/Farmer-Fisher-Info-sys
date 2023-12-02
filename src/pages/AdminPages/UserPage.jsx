e0a6d03f7e4d28214e69986c0c0efa45d4dd352a
raaaaaaaaaf <rridad@ssct.edu.ph>
2023-12-02 08:34:22 +0800

    Fix user Table

--- src/pages/AdminPages/UserPage.jsx
+++ src/pages/AdminPages/UserPage.jsx
@@ -1,7 +1,7 @@
-import { Helmet } from "react-helmet-async";
-import { filter } from "lodash";
-import { sentenceCase } from "change-case";
-import { useEffect, useState } from "react";
+import { Helmet } from 'react-helmet-async';
+import { filter } from 'lodash';
+import { sentenceCase } from 'change-case';
+import { useEffect, useState } from 'react';
 // @mui
 import {
   Card,
@@ -21,27 +21,28 @@ import {
   IconButton,
   TableContainer,
   TablePagination,
-} from "@mui/material";
+} from '@mui/material';
 // components
-
-import Iconify from "../../components/iconify";
-import Scrollbar from "../../components/scrollbar";
+import Label from '../../components/label';
+import Iconify from '../../components/iconify';
+import Scrollbar from '../../components/scrollbar';
 // sections
-import { UserListHead, UserListToolbar } from "../../sections/@dashboard/user";
+import { UserListHead, UserListToolbar } from '../../sections/@dashboard/user';
 // mock
-
-import { collection, getDoc, getDocs, doc, deleteDoc } from "firebase/firestore";
-import { db } from "../../firebase/firebaseConfig";
-import Loading from "../../components/loading/Loading";
-import Swal from "sweetalert2";
+import USERLIST from '../../_mock/user';
+import { collection, getDoc, getDocs, doc, deleteDoc } from 'firebase/firestore';
+import { db } from '../../firebase/firebaseConfig';
+import Loading from '../../components/loading/Loading';
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
 
 // ----------------------------------------------------------------------
 
 const TABLE_HEAD = [
-  { id: "name", label: "Name", alignRight: false },
-  { id: "email", label: "E-mail", alignRight: false },
-  { id: "role", label: "Role", alignRight: false },
-  { id: "act", label: "Action", alignRight: false },
+  { id: 'name', label: 'Name', alignRight: false },
+  { id: 'email', label: 'E-mail', alignRight: false },
+  { id: 'role', label: 'Role', alignRight: false },
+  { id: 'act', label: 'Action', alignRight: false },
 ];
 
 // ----------------------------------------------------------------------
@@ -57,7 +58,7 @@ function descendingComparator(a, b, orderBy) {
 }
 
 function getComparator(order, orderBy) {
-  return order === "desc"
+  return order === 'desc'
     ? (a, b) => descendingComparator(a, b, orderBy)
     : (a, b) => -descendingComparator(a, b, orderBy);
 }
@@ -70,10 +71,7 @@ function applySortFilter(array, comparator, query) {
     return a[1] - b[1];
   });
   if (query) {
-    return filter(
-      array,
-      (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1
-    );
+    return filter(array, (_user) => _user.displayName.toLowerCase().indexOf(query.toLowerCase()) !== -1);
   }
   return stabilizedThis.map((el) => el[0]);
 }
@@ -83,43 +81,46 @@ export default function UserPage() {
 
   const [page, setPage] = useState(0);
 
-  const [order, setOrder] = useState("asc");
+  const [order, setOrder] = useState('asc');
 
   const [selected, setSelected] = useState([]);
 
-  const [orderBy, setOrderBy] = useState("name");
+  const [orderBy, setOrderBy] = useState('name');
 
-  const [filterName, setFilterName] = useState("");
+  const [filterName, setFilterName] = useState('');
 
   const [rowsPerPage, setRowsPerPage] = useState(5);
 
-  const [userList, setUserList] = useState([]);
+  const [userList, setUserList] = useState([])
 
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true)
 
-  const userRef = collection(db, "users");
+  const userRef = collection(db, "users")
+
+  const navigate = useNavigate()
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await getDocs(userRef);
-        const filteredData = data.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        setUserList(filteredData);
-      } catch (err) {
+       const data = await getDocs(userRef)
+       const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setUserList(filteredData)
+
+      } catch(err) {
         console.error(err);
       }
-    };
-    fetchData();
-  }, []);
+    }
+    fetchData()
+  }, [])
 
   useEffect(() => {
     setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
+      setLoading(false)
+    }, 2000)
+  }, [])
 
   const deleteProfile = async (id) => {
     try {
@@ -132,6 +133,7 @@ export default function UserPage() {
         showConfirmButton: false,
         timer: 1500,
       });
+      navigate('/dashboard/user')
     } catch (error) {
       // Handle any errors that may occur during deletion
       console.error("Error deleting user profile:", error);
@@ -149,8 +151,8 @@ export default function UserPage() {
   };
 
   const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
     setOrderBy(property);
   };
 
@@ -173,10 +175,7 @@ export default function UserPage() {
     } else if (selectedIndex === selected.length - 1) {
       newSelected = newSelected.concat(selected.slice(0, -1));
     } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1)
-      );
+      newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
     }
     setSelected(newSelected);
   };
@@ -195,108 +194,66 @@ export default function UserPage() {
     setFilterName(event.target.value);
   };
 
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - userList.length) : 0;
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - userList.length) : 0;
 
-  const filteredUsers = applySortFilter(
-    userList,
-    getComparator(order, orderBy),
-    filterName
-  );
+  const filteredUsers = applySortFilter(userList, getComparator(order, orderBy), filterName);
 
   const isNotFound = !filteredUsers.length && !!filterName;
 
   return (
     <>
       <Helmet>
-        <title> User | Local Farmers and Fisher folks web-based information system </title>
+        <title> User | Minimal UI </title>
       </Helmet>
 
       <Container>
-        <Stack
-          direction="row"
-          alignItems="center"
-          justifyContent="space-between"
-          mb={5}
-        >
+        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
           <Typography variant="h4" gutterBottom>
             User
           </Typography>
         </Stack>
-        {loading ? (
-          <Loading />
-        ) : (
-          <Card>
-            <UserListToolbar
-              numSelected={selected.length}
-              filterName={filterName}
-              onFilterName={handleFilterByName}
-            />
+      {loading ? (
+        <Loading/>
+      ) : (
+        <Card>
+        <UserListToolbar numSelected={selected.length} filterName={filterName} onFilterName={handleFilterByName} />
 
-            <Scrollbar>
-              <TableContainer sx={{ minWidth: 800 }}>
-                <Table>
-                  <UserListHead
-                    order={order}
-                    orderBy={orderBy}
-                    headLabel={TABLE_HEAD}
-                    rowCount={userList.length}
-                    numSelected={selected.length}
-                    onRequestSort={handleRequestSort}
-                    onSelectAllClick={handleSelectAllClick}
-                  />
-                  <TableBody>
-                    {userList
-                      .slice(
-                        page * rowsPerPage,
-                        page * rowsPerPage + rowsPerPage
-                      )
-                      .map((user, index) => {
-                        const selectedUser =
-                          selected.indexOf(user.displayName) !== -1;
+        <Scrollbar>
+          <TableContainer sx={{ minWidth: 800 }}>
+            <Table>
+              <UserListHead
+                order={order}
+                orderBy={orderBy}
+                headLabel={TABLE_HEAD}
+                rowCount={userList.length}
+                numSelected={selected.length}
+                onRequestSort={handleRequestSort}
+                onSelectAllClick={handleSelectAllClick}
+              />
+              <TableBody>
+                {filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((user, index) => {
+                  const selectedUser = selected.indexOf(user.displayName) !== -1;
 
-                        return (
-                          <TableRow
-                            hover
-                            key={index}
-                            tabIndex={-1}
-                            role="checkbox"
-                            selected={selectedUser}
-                          >
-                            <TableCell padding="checkbox">
-                              <Checkbox
-                                checked={selectedUser}
-                                onChange={(event) => handleClick(event, name)}
-                              />
-                            </TableCell>
+                  return (
+                    <TableRow hover key={index} tabIndex={-1} role="checkbox" selected={selectedUser}>
+                      <TableCell padding="checkbox">
+                        <Checkbox checked={selectedUser} onChange={(event) => handleClick(event, name)} />
+                      </TableCell>
 
-                            <TableCell
-                              component="th"
-                              scope="row"
-                              padding="none"
-                            >
-                              <Stack
-                                direction="row"
-                                alignItems="center"
-                                spacing={2}
-                              >
-                                <Avatar
-                                  alt={user.displayName}
-                                  src={user.photoURL ?? `/assets/images/avatars/avatar_${
-                                    index + 1
-                                  }.jpg`}
-                                />
-                                <Typography variant="subtitle2" noWrap>
-                                  {user.displayName}
-                                </Typography>
-                              </Stack>
-                            </TableCell>
+                      <TableCell component="th" scope="row" padding="none">
+                        <Stack direction="row" alignItems="center" spacing={2}>
+                          <Avatar alt={user.displayName} src={user.photoURL ?? `/assets/images/avatars/avatar_${index + 1}.jpg`} />
+                          <Typography variant="subtitle2" noWrap>
+                            {user.displayName}
+                          </Typography>
+                        </Stack>
+                      </TableCell>
 
-                            <TableCell align="left">{user.email}</TableCell>
+                      <TableCell align="left">{user.email}</TableCell>
 
-                            <TableCell align="left">{user.role}</TableCell>
+                      <TableCell align="left">{user.role}</TableCell>
 
-                            <TableCell>
+                      <TableCell>
                             <IconButton
                                 size="large"
                                 color="inherit"
@@ -307,85 +264,57 @@ export default function UserPage() {
                                 />
                               </IconButton>
                             </TableCell>
-                          </TableRow>
-                        );
-                      })}
-                    {emptyRows > 0 && (
-                      <TableRow style={{ height: 53 * emptyRows }}>
-                        <TableCell colSpan={6} />
-                      </TableRow>
-                    )}
-                  </TableBody>
+                    </TableRow>
+                  );
+                })}
+                {emptyRows > 0 && (
+                  <TableRow style={{ height: 53 * emptyRows }}>
+                    <TableCell colSpan={6} />
+                  </TableRow>
+                )}
+              </TableBody>
 
-                  {isNotFound && (
-                    <TableBody>
-                      <TableRow>
-                        <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
-                          <Paper
-                            sx={{
-                              textAlign: "center",
-                            }}
-                          >
-                            <Typography variant="h6" paragraph>
-                              Not found
-                            </Typography>
+              {isNotFound && (
+                <TableBody>
+                  <TableRow>
+                    <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
+                      <Paper
+                        sx={{
+                          textAlign: 'center',
+                        }}
+                      >
+                        <Typography variant="h6" paragraph>
+                          Not found
+                        </Typography>
 
-                            <Typography variant="body2">
-                              No results found for &nbsp;
-                              <strong>&quot;{filterName}&quot;</strong>.
-                              <br /> Try checking for typos or using complete
-                              words.
-                            </Typography>
-                          </Paper>
-                        </TableCell>
-                      </TableRow>
-                    </TableBody>
-                  )}
-                </Table>
-              </TableContainer>
-            </Scrollbar>
+                        <Typography variant="body2">
+                          No results found for &nbsp;
+                          <strong>&quot;{filterName}&quot;</strong>.
+                          <br /> Try checking for typos or using complete words.
+                        </Typography>
+                      </Paper>
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              )}
+            </Table>
+          </TableContainer>
+        </Scrollbar>
 
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25]}
-              component="div"
-              count={userList.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-          </Card>
-        )}
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={userList.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Card>
+
+      )}
+
       </Container>
-
-      <Popover
-        open={Boolean(open)}
-        anchorEl={open}
-        onClose={handleCloseMenu}
-        anchorOrigin={{ vertical: "top", horizontal: "left" }}
-        transformOrigin={{ vertical: "top", horizontal: "right" }}
-        PaperProps={{
-          sx: {
-            p: 1,
-            width: 140,
-            "& .MuiMenuItem-root": {
-              px: 1,
-              typography: "body2",
-              borderRadius: 0.75,
-            },
-          },
-        }}
-      >
-        <MenuItem>
-          <Iconify icon={"eva:edit-fill"} sx={{ mr: 2 }} />
-          Edit
-        </MenuItem>
-
-        <MenuItem sx={{ color: "error.main" }}>
-          <Iconify icon={"eva:trash-2-outline"} sx={{ mr: 2 }} />
-          Delete
-        </MenuItem>
-      </Popover>
     </>
   );
 }
